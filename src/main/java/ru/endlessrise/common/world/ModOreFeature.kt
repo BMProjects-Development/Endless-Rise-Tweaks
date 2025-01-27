690cd89400785e46885c5bafaac04a35d86cb25d
BarMaxx <98416932+BarMaxx@users.noreply.github.com>
2025-01-27 18:31:40 +0300

    Some changes

---
+++ src/main/java/ru/endlessrise/common/world/ModOreFeature.kt
@@ -0,0 +1,9 @@
+package ru.endlessrise.common.world
+
+import net.minecraft.world.gen.feature.ConfiguredFeature
+
+object ModOreFeature {
+
+    //val ConfiguredFeature<?, ?> CHLOROPHYTE = Feature.ORE
+            //.withCon
+}
