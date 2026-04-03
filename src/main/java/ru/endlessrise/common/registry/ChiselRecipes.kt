167f17cf4489f1b52a8660148b8c31dfa10f4833
BarMaxx <98416932+BarMaxx@users.noreply.github.com>
2026-04-03 18:51:24 +0300

    Added atum & curios

--- src/main/java/ru/endlessrise/common/registry/ChiselRecipes.kt
+++ src/main/java/ru/endlessrise/common/registry/ChiselRecipes.kt
@@ -1,5 +1,6 @@
 package ru.endlessrise.common.registry
 
+import com.teammetallurgy.atum.init.AtumBlocks
 import net.minecraft.block.Blocks
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
@@ -8,19 +9,11 @@ import ru.endlessrise.common.recipes.ChiselRecipe
 object ChiselRecipes {
     val RECIPES = arrayListOf(
         ChiselRecipe(
-            Blocks.GOLD_BLOCK,
-            ItemStack(Items.DIAMOND_BLOCK),
-            ItemStack(Items.STICK, 5),
+            Blocks.SANDSTONE,
+            ItemStack(AtumBlocks.QUERN),
+            ItemStack(Items.STICK, 4),
             10,
-            offhandItem = ItemStack(Items.FLINT, 2)
-        ),
-
-        ChiselRecipe(
-            Blocks.SAND,
-            ItemStack(Items.SANDSTONE),
-            ItemStack(Items.FLINT, 3),
-            8,
-            offhandItem = ItemStack(Items.STICK, 2)
+            offhandItem = ItemStack(Items.FLINT, 3)
         )
     )
 }
